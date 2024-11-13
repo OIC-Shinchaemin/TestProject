4b23a8d7690c213bbae5a9eff24888db5a784d46
shin <shin@gmail.com>
2024-11-13 09:56:19 +0900

    ステージベーススクリプト作成

---
+++ Assets/Scripts/StageScroll.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageScroll : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Ç»Ç…Ç©Ç‚ÇËÇÃÇ±ÇµÇ™Ç†ÇÈ
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Ç‡Ç§àÍÇ¬Ç†Ç¡ÇΩÅI
+    }
+}
